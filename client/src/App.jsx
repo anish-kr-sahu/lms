6a75c31d8334f1c2b3a236338e84a0fef7164926
anish <anishkr0098@gmail.com>
2025-11-21 09:53:32 +0530

    initialize the project

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        Hello World
+        <Button>Let's Build Lms 🙂</Button>
+      </div>
+        
+    </>
+  )
+}
+
+export default App
